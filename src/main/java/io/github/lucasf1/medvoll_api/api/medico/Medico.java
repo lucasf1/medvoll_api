872fa057e651cccd8b2266dfd28ce433508e8b39
Lucas Farias <lucasf1@gmail.com>
2023-01-13 18:20:01 -0300

    Criação da entidade Medico e do repositorio MedicoRepositorio.

---
+++ src/main/java/io/github/lucasf1/medvoll_api/api/medico/Medico.java
@@ -0,0 +1,45 @@
+package io.github.lucasf1.medvoll_api.api.medico;
+
+import io.github.lucasf1.medvoll_api.api.endereco.Endereco;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
--- src/main/java/io/github/lucasf1/medvoll_api/controller/MedicoController.java
+++ src/main/java/io/github/lucasf1/medvoll_api/controller/MedicoController.java
@@ -1,18 +1,24 @@
 package io.github.lucasf1.medvoll_api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.lucasf1.medvoll_api.api.paciente.DadosCadastroPaciente;
+import io.github.lucasf1.medvoll_api.api.medico.DadosCadastroMedico;
+import io.github.lucasf1.medvoll_api.api.medico.Medico;
+import io.github.lucasf1.medvoll_api.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
     
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println("dados recebido: " +dados);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
     }
 }
