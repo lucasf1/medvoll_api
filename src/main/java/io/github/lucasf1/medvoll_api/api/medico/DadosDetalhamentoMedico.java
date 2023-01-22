0e9b8f43306e3c173af8eb2524491f6b0a0e97b6
Lucas Farias <lucasf1@gmail.com>
2023-01-22 08:46:12 -0300

    Utilização da classe ResponseEntity no retorno do MedicoController.

---
+++ src/main/java/io/github/lucasf1/medvoll_api/api/medico/DadosDetalhamentoMedico.java
@@ -0,0 +1,18 @@
+package io.github.lucasf1.medvoll_api.api.medico;
+
+import io.github.lucasf1.medvoll_api.api.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(
+        Long id,
+        String nome,
+        String email,
+        String crm,
+        String telefone,
+        Especialidade especialidade,
+        Endereco endereco) {
+
+    public DadosDetalhamentoMedico(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(),
+                medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+    }
+}
--- src/main/java/io/github/lucasf1/medvoll_api/controller/MedicoController.java
+++ src/main/java/io/github/lucasf1/medvoll_api/controller/MedicoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import io.github.lucasf1.medvoll_api.api.medico.DadosAtualizacaoMedico;
 import io.github.lucasf1.medvoll_api.api.medico.DadosCadastroMedico;
+import io.github.lucasf1.medvoll_api.api.medico.DadosDetalhamentoMedico;
 import io.github.lucasf1.medvoll_api.api.medico.DadosListagemMedico;
 import io.github.lucasf1.medvoll_api.api.medico.Medico;
 import io.github.lucasf1.medvoll_api.api.medico.MedicoRepository;
@@ -30,27 +33,49 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-        repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(
+            @RequestBody @Valid DadosCadastroMedico dados,
+            UriComponentsBuilder uriBuilder) {
+        var medico = new Medico(dados);
+        repository.save(medico);
+
+        var uri = uriBuilder.path("/medicos/{id}")
+                .buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity<Page<DadosListagemMedico>> listar(
+            @PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao)
+                .map(DadosListagemMedico::new);
+
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
-
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medicoBD = repository.getReferenceById(dados.id());
         medicoBD.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medicoBD));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         var medicoBD = repository.getReferenceById(id);
         medicoBD.excluir();
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medicoBD = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medicoBD));
     }
 }
