211db6084bfaca2a335a12c1d31fbd84f4cc7111
Lucas Farias <lucasf1@gmail.com>
2023-01-28 10:13:37 -0300

    Uso do token para a autenticação.

--- src/main/java/io/github/lucasf1/medvoll_api/api/infra/security/TokenService.java
+++ src/main/java/io/github/lucasf1/medvoll_api/api/infra/security/TokenService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import io.github.lucasf1.medvoll_api.api.domain.usuario.Usuario;
 
@@ -29,6 +30,19 @@ public class TokenService {
                 .sign(algoritmo);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                .withIssuer("API Voll.med")
+                .build()
+                .verify(tokenJWT)
+                .getSubject();            
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
